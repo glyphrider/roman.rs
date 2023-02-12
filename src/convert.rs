dbdb50ec7656978525a0af45d2c67946c5d25425
Brian H. Ward <glyphrider@gmail.com>
2023-02-12 11:16:22 -0500

    renamed some (private) functions
    more descriptive names made more sense when looking back at the code

--- src/convert.rs
+++ src/convert.rs
@@ -8,13 +8,14 @@ pub fn to(arabic: u16) -> String {
 
     for e in translate.iter().rev() {
         let (arabic_value,roman_value) = e;
-        to_n(&mut working_arabic, &mut result, *arabic_value, roman_value);
+        // pulling references out of the iterator means we have to deref arabic
+        apply_translate_element_to_arabic_value(&mut working_arabic, &mut result, *arabic_value, roman_value);
     }
     return result
 }
 
-fn to_n(arabic: &mut u16, result: &mut String, arabic_value: u16, roman_value: &str) {
-    
+fn apply_translate_element_to_arabic_value(arabic: &mut u16, result: &mut String, arabic_value: u16, roman_value: &str) {
+    // receiving arabic as a mut u16 ref means we have to deref it to use it as a u16 -- but we're accessing the original memory!
     while *arabic >= arabic_value {
         *arabic -= arabic_value;
         result.push_str(roman_value);
@@ -29,12 +30,12 @@ pub fn from(roman: &str) -> u16 {
 
     for e in translate.iter().rev() {
         let (arabic_value,roman_value) = e;
-        from_n(&mut working_roman, &mut result, *arabic_value, roman_value);
+        apply_translate_element_to_roman_value(&mut working_roman, &mut result, *arabic_value, roman_value);
     }
     return result;
 }
 
-fn from_n(roman: &mut String, result: &mut u16, arabic_value: u16, roman_value: &str) {
+fn apply_translate_element_to_roman_value(roman: &mut String, result: &mut u16, arabic_value: u16, roman_value: &str) {
     while roman.starts_with(roman_value) {
         let working_roman: String = roman.clone();
         roman.clear();
